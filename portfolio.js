df3c99158503f1725ba72a4beefaa34d5953febd
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-08-05 10:46:19 +0200

    adding gsap animation to logo

--- portfolio.js
+++ portfolio.js
@@ -1,3 +1,11 @@
 //---- start here
 
 console.log("I'm in!!");
+
+const tl = gsap.timeline();
+
+//tl.from(".logo", {opacity:0});
+tl.from(".logo-svg g" , {x:50, opacity:0, stopColor: "#333", ease: "Back.easeOut", stagger:{
+  amount:0.8,
+  from:"start"
+}, delay:1});
