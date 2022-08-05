4338ba103378896c6d2b95998cd9191ed7790119
Jasin Tairaidrissi <jasin.tairaidrissi@gmail.com>
2022-08-05 10:53:23 +0200

    speeding up logo anim

--- portfolio.js
+++ portfolio.js
@@ -6,6 +6,6 @@ const tl = gsap.timeline();
 
 //tl.from(".logo", {opacity:0});
 tl.from(".logo-svg g" , {x:50, opacity:0, stopColor: "#333", ease: "Back.easeOut", stagger:{
-  amount:0.8,
+  amount:0.5,
   from:"start"
 }, delay:1});
